4422efdbec2e0dddf173e2ce4317bb8de11b4770
kistapir1 <kistapir0@gmail.com>
2021-10-12 15:35:29 +0200

    Cs xd lol kancsalcigo

--- core/src/csakennijottunk/InGame/InActors.java
+++ core/src/csakennijottunk/InGame/InActors.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
 import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
+import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
 
 public class InActors extends OneSpriteStaticActor {
     ClickListener c1;
@@ -19,5 +20,13 @@ public class InActors extends OneSpriteStaticActor {
                 game.setScreen(new RodShopScreen(game));
             }
         });
+    }
+}
+
+class Header extends OneSpriteStaticActor {
+    public Header(MyGame game) {
+        super(game, "fejlec.png");
+        this.setSize(90,10);
+        this.setPosition(0,90 - 10);
     }
 }
---
+++ core/src/csakennijottunk/InGame/Ls.java
@@ -0,0 +1,16 @@
+package csakennijottunk.InGame;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+import hu.csanyzeg.master.MyBaseClasses.Assets.MyAssetManager;
+import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
+import hu.csanyzeg.master.MyBaseClasses.UI.MyLabel;
+
+public class Ls extends Label.LabelStyle {
+    public Ls(MyGame game) {
+        super(game.getMyAssetManager().getFont("alegreyaregular.otf"), Color.WHITE);
+    }
+}
