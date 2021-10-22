7d3867cc2bb6b7e617a69ea84fa8ee918f3d0f52
tuskeb <tuskebganz@gmail.com>
2021-10-22 16:26:21 +0200

    Zsinor

--- core/src/csakennijottunk/InGame/BaitActor.java
+++ core/src/csakennijottunk/InGame/BaitActor.java
@@ -10,27 +10,13 @@ import hu.csanyzeg.master.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 import hu.csanyzeg.master.MyBaseClasses.SimpleWorld.SimpleOverlapsUtil;
 import hu.csanyzeg.master.MyBaseClasses.SimpleWorld.SimpleWorld;
 
-public class BaitActor extends OneSpriteStaticActor {
-    protected Ballistics2 ballistics2;
-    protected float waterHeight;
-    protected boolean flying = true;
-    protected float speed = 5;
-    protected Vector2 endpoint;
-    protected Vector2 startpoint;
-    protected float falltime;
+public class BaitActor extends BallisticActor {
 
     protected Vector<WhiteActor> whiteActorVector = new Vector<>();
 
     public BaitActor(MyGame game, Ballistics2 ballistics, float waterHeight) {
-        super(game, "Bait.png");
-        this.ballistics2 = ballistics;
-        this.waterHeight = waterHeight;
-        startpoint = new Vector2(ballistics.getX0(), ballistics.getY0());
-        endpoint = new Vector2(ballistics2.getPosition(ballistics2.getElapsedTimeFromY(waterHeight)[1]).x, ballistics2.getPosition(ballistics2.getElapsedTimeFromY(waterHeight)[1]).y);
-        falltime = ballistics2.getElapsedTimeFromY(waterHeight)[1];
+        super(game, "Bait.png", ballistics, waterHeight);
         setSize(128,128);
-
-
     }
 
     @Override
@@ -46,12 +32,6 @@ public class BaitActor extends OneSpriteStaticActor {
             );
             whiteActorVector.add(w);
             getStage().addActor(w);
-            if (getX() + getWidth() / 2 < ballistics2.getX(waterHeight)[1]) {
-                setPosition(ballistics2.getPosition(getElapsedTime() * speed).x - getWidth() / 2, ballistics2.getPosition(getElapsedTime() * speed).y - getHeight() / 2);
-            } else {
-                setPosition(endpoint.x - getWidth() / 2, endpoint.y - getHeight() / 2);
-                flying = false;
-            }
         }
     }
 
