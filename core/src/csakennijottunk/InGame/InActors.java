8746ad3fc869ba0bb2ed5756adba27d6ad52c4e0
kistapir1 <kistapir0@gmail.com>
2021-10-22 12:50:11 +0200

    HandRod actor

--- core/src/csakennijottunk/InGame/InActors.java
+++ core/src/csakennijottunk/InGame/InActors.java
@@ -159,6 +159,14 @@ class ShopBg extends OneSpriteStaticActor {
     }
 }
 
+class HandRod extends OneSpriteStaticActor {
+    public HandRod(MyGame game) {
+        super(game, "HandRod.png");
+        int size = 90;
+        this.setSize(size,80);
+    }
+}
+
 class Header extends OneSpriteStaticActor {
     public Header(MyGame game) {
         super(game, "fejlec.png");
