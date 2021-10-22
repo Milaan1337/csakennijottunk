80ed781599550f4d605835b5ce19b907835946e5
tuskeb <tuskebganz@gmail.com>
2021-10-22 19:08:07 +0200

    Felbntas

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,9 +9,8 @@ import csakennijottunk.Menu.MainGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MainGame(false), config);
-
 		config.width = 1280;
-		config.height = 720;
+		config.height = 520;
+		new LwjglApplication(new MainGame(false), config);
 	}
 }
